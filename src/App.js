24f909b4e892ec026acb284fb34b33725ef31dde
Isesin <ignasesin@gmail.com>
2022-01-26 19:39:23 -0300

    NavBar creation

--- src/App.js
+++ src/App.js
@@ -1,35 +1,60 @@
-
 import "./App.css";
+import NavBar from "./components/NavBar.js";
 
-const product = "E-Commerce"
-const welcome = "Bienvenidos"
+const product = "E-Commerce";
+const welcome = "Bienvenidos";
 
 const styles = {
   welcome: {
-    backgroundColor: 'yellow',
-    color: 'red',
-    border: '10px solid black',
-    width: 'auto',
-    height: 'auto',
-    margin: '5 5px',
-    padding: '5',
+    backgroundColor: "yellow",
+    color: "red",
+    border: "10px solid black",
+    width: "auto",
+    height: "auto",
+    margin: "5 5px",
+    padding: "5",
   },
 };
 
-function App() {
+/* COMPONENTE DE FUNCION / FUNCIONALES/ COOKS */
+/* EXISTEN COMPONENTES DE CLASE TAMBIEN */
+
+function App ()  {
   return (
-    <div className="App">
-      <section className="container">
-        <h1 className="title">Mi primer {product}</h1>
-        <h2 className="welcome" style={styles.welcome}>{welcome}, disfruten de nuestros servicios!</h2>
-      </section>
-      <section>
-    <p className="texto" style={styles.welcome}> 
-    Ponele que me toma el Lorem ipsum pero no! 
-    </p>
-      </section>
-    </div>
+    <>
+      <NavBar/>
+      <div className="App">
+        <section className="container">
+          <h1 className="title">Mi primer {product}</h1>
+          <h2 className="welcome" style={styles.welcome}>
+            {welcome}, disfruten de nuestros servicios!
+          </h2>
+          <h3 className="title3">Titulo Relleno</h3>
+        </section>
+        <section>
+          <p className="texto" style={styles.welcome}>
+            Ponele que me toma el Lorem ipsum pero no!
+          </p>
+        </section>
+      </div>
+    </>
   );
+};
+
+export default App; /* EXPORT DEFAULT hace visible la funcion*/
+
+/*
+FUNCION DECLARADA:
+function foo() {
+  return 5
+}
+*/
+/*
+EXPRESION FUNCIONAL
+const myFunction = () => {
+  return 5
 }
 
-export default App;
+Y LA LLAMO:
+myFunction()
+*/
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+
+import "./NavBar.css"
+
+const NavBar = () => {
+    return 
+    <nav className= "Navigation">
+        <ul className>
+            <li>Ejemplo</li>
+            <li>Ejemplo</li>
+            <li>Ejemplo</li>
+        </ul>
+    </nav>
+
+}
+
+export default NavBar;
